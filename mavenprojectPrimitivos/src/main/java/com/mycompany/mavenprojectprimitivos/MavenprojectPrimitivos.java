0337519544fd0cb05fa4b86a98d261705a763e0e
Leandro Ferreira <leanfj@hotmail.com>
2022-10-17 23:00:03 -0300

    Criando classe - memoria

---
+++ mavenprojectPrimitivos/src/main/java/com/mycompany/mavenprojectprimitivos/MavenprojectPrimitivos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenprojectprimitivos;
+
+/**
+ *
+ * @author leandro
+ */
+public class MavenprojectPrimitivos {
+
+    public static void main(String[] args) {
+        int p = 0; /* int p ;*/
+        System.out.println(p);
+        
+        p = 10;
+        
+        System.out.println(p);
+    }
+}
